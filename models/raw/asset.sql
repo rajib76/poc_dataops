e6c803fcd4ebadbb35a0d31910806d3dcb383a66
RAJIB DEB <rajib_deb@infosys.com>
2021-03-12 16:50:54 +0000

    TEST

---
+++ models/raw/asset.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='table', transient=false) }}
+
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from raw.jaffle_shop.customers
---
+++ models/raw/test.sql
@@ -0,0 +1,1 @@
+{{create_table(operation='ALTER',col='TEST',datatype='VARCHAR')}}
